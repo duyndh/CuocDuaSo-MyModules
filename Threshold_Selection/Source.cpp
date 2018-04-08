944ae585e420d39a98bd1822cb8c81b2ce0a64b9
duyndh <hoangduypok98@gmail.com>
2018-04-08 13:37:29 +0700

    Test OpenCV

--- Threshold_Selection/Source.cpp
+++ Threshold_Selection/Source.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
-	cout << "Hello World !";
+	Mat img = imread("img.jpg");
+	imshow("image", img);
+	waitKey(0);
+
 	return 0;
 }
